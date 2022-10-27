8db153655e2619181e3c255d7a92846063a44a01
Vinicius Fernandes <viniciusfernoli@gmail.com>
2022-10-26 21:03:39 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+    <h1 className='font-bold text-2xl text-gray-100'>Oi mundo DEV</h1>
+    <button className='text-white bg-cyan-500 font-medium hover:bg-cyan-300 px-4 py-2 rounded'>Enviar</button>
+    </>
+  )
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import {clsx} from 'clsx';
+import { ReactNode } from 'react';
+import {Slot} from '@radix-ui/react-slot'
+
+export interface ButtonProps {
+  size?: 'sm' | 'md' | 'lg',
+  children: ReactNode,
+  asChild?: boolean, 
+}
+export function Button({size = 'md', children, asChild}: ButtonProps) {
+  const Comp = asChild ? Slot : 'button'; 
+  return (
+    <Comp className={clsx(
+      'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors focus:ring-2 ring-white hover:bg-cyan-300'
+    )}
+    > 
+      {children} 
+    </Comp>
+  )
+}
